b269cbc0bc62520e9e92e8023528118dc6fc3709
NDDEVVlog <nddevgame@gmail.com>
2026-09-03 17:15:02 +0700

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 // src/App.jsx
 
 import React, { useState, useRef, useEffect } from 'react';
-// --- THAY ĐỔI Ở ĐÂY: Đổi BrowserRouter thành HashRouter ---
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FaEnvelope, FaDiscord } from 'react-icons/fa';
 
 // Components
